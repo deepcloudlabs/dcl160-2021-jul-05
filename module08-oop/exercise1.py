e4fb27775ef1fddcdf2ae78d47801a0c41e316b5
deepcloudlabs <deepcloudlabs@gmail.com>
2021-07-08 10:46:27 +0300

    initial commit

--- module08-oop/exercise1.py
+++ module08-oop/exercise1.py
@@ -1,3 +1,11 @@
+"""
+    OOP:   i) Class/Object -> Encapsulation
+          ii) Inheritance  -> Re-usability *
+         iii) Polymorphism -> Agility
+"""
+from typing import Type
+
+
 class InsufficientBalanceException(Exception):
     def __init__(self, reason, deficit):
         self.reason = reason
@@ -47,8 +55,28 @@ class account(object):
         return float(self.balance)
 
 
+# account          -> base class, super class
+# checking_account -> sub class, derived class
+class checking_account(account):
+    def __init__(self, iban, balance=0, overdraft_amount=500):
+        super().__init__(iban, balance)
+        self.overdraft_amount = overdraft_amount
+
+    def withdraw(self, amount):  # overriding
+        if amount <= 0:  # validation
+            raise ValueError(f"{amount} must be positive.")
+        if amount > (self.balance + self.overdraft_amount):  # business rule
+            raise InsufficientBalanceException(reason="Your balance does not cover your expenses."
+                                               , deficit=amount - self.balance - self.overdraft_amount)
+        self.balance -= amount
+
+    def __str__(self):  # overriding
+        return f"checking_account (iban={self.iban}, balance={self.balance}, overdraft_amount={self.overdraft_amount})"
+
+
 try:
     acc1 = account(iban="tr1", balance=10_000)
+    acc2 = checking_account(iban="tr2", balance=20_000, overdraft_amount=1_000)
     print(acc1)
     print(float(acc1))
     acc1.withdraw(2_500)
@@ -57,7 +85,20 @@ try:
     print(f"balance is {acc1.balance}")
     acc1.withdraw(12_500)
     print(f"balance is {acc1.balance}")
-    acc1.withdraw(-1)
+    acc2.withdraw(21_000)
+    print(acc2)
+    print(isinstance(acc1, account))  # True
+    print(isinstance(acc1, checking_account))  # False
+    print(isinstance(acc2, checking_account))  # True
+    print(isinstance(acc2, account))  # True ? False -> True
+    # acc2 -> class checking_account(account) -> class account(object) -> object
+    print(isinstance(acc1, object))  # True ? False -> True
+    print(isinstance(acc2, object))  # True ? False -> True
+    print(isinstance(42, object))  # True ? False -> True : 42 -> int -> object
+    print(isinstance(42, int))  # True ? False
+    print(isinstance(True, bool))  # True ? False
+    print(isinstance(3.1415, float))  # True ? False
+    print(isinstance("jack shephard", str))  # True ? False
 except ValueError as err:
     print(err)
 except InsufficientBalanceException as err:
